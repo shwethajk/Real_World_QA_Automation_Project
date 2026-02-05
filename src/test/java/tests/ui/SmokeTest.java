309a8063b35fe51becefe6f993d7f0adda75ee8d
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-02-05 11:00:05 +0530

    Add SmokeTests class with page title verification

--- src/test/java/tests/ui/SmokeTest.java
+++ src/test/java/tests/ui/SmokeTest.java
@@ -1,1 +1,14 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SmokeTests extends BaseTest {
+
+    @Test
+    public void verifyPageTitle() {
+        String title = driver.getTitle();
+        Assert.assertTrue(title.length() > 0, "Title should not be empty");
+    }
+}
 // Smoke UI tests
