d39c859310642f59d2036ebdc1dbef9fa700ab12
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-02-05 12:09:29 +0530

    Change package declaration from tests to tests.ui

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,4 @@
-package tests;
+package tests.ui;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,4 +23,3 @@ public class BaseTest {
         }
     }
 }
-// Grid + Driver Factory
